1da2042c2535f9299580ba678b503d75c3e56538
raffaele-romeo <raffaele.romeo.91.rr@gmail.com>
2021-07-21 00:28:31 +0100

    improved solution

--- src/main/scala/graph/FindTheNearestClone.scala
+++ src/main/scala/graph/FindTheNearestClone.scala
@@ -1,68 +1,73 @@
 package graph
 
-import scala.collection.mutable.{Map => MMap, Queue => MQueue, ListBuffer => MList}
+import scala.annotation.tailrec
+import scala.collection.mutable.{ListBuffer => MList, Map => MMap, Queue => MQueue}
 
 object FindTheNearestClone {
 
+  type Node = Int
+  type Graph = Map[Node, List[Node]]
+
+  def addNodesToGraph(graph: Graph, graphFrom: Array[Int], graphTo: Array[Int]): Graph = {
+    @tailrec
+    def loop(graph: Graph, edges: List[(Node, Node)]): Graph = {
+      edges match {
+        case Nil => graph
+        case head :: tail =>
+          val newGraph = addNode(head._1, head._2).compose(addNode(head._2, head._1))(graph)
+          loop(newGraph, tail)
+      }
+    }
+
+    def addNode(node1: Node, node2: Node): Graph => Graph =
+      graph => graph + ((node1, node2 :: graph.getOrElse(node1, List.empty[Node])))
+
+    val edges: Array[(Node, Node)] = graphFrom.zip(graphTo)
+    loop(graph, edges.toList)
+  }
+
   def findShortest(graphNodes: Int, graphFrom: Array[Int], graphTo: Array[Int], ids: Array[Long], value: Int): Int = {
 
-    type Node = Int
-    type Graph = MMap[Node, List[Node]]
+    val graph = addNodesToGraph(Map.empty[Node, List[Node]], graphFrom, graphTo)
 
-    val graph: Graph = MMap.empty[Node, List[Node]]
-
-    def addElementToMap(key: Int, value: Int): Graph = {
-      graph.addOne((key, value :: graph.getOrElse(key, List.empty[Node])))
-    }
-
-    for (i <- graphFrom.indices) {
-      addElementToMap(graphFrom(i), graphTo(i))
-      addElementToMap(graphTo(i), graphFrom(i))
-    }
-
-    val elementsWithSameColor = for {
+    val nodesWithSameColourAsValue = for {
       i <- ids.indices if ids(i) == value
     } yield i + 1
 
 
-    if (elementsWithSameColor.size < 2)
-      -1
-    else {
-      val startNode = elementsWithSameColor.head
-      val otherNodes = elementsWithSameColor.tail.toList
+    if (nodesWithSameColourAsValue.size >= 2) {
+      val startNode = nodesWithSameColourAsValue.head
+      val otherNodes = nodesWithSameColourAsValue.tail.toList
 
       val queue = MQueue(startNode)
       val visited = MList(startNode)
       var output = 0
-      var foundValue = false
+      var isNearestClone = false
 
-      while (queue.nonEmpty && !foundValue) {
+      while (queue.nonEmpty && !isNearestClone) {
         val n = queue.dequeue()
-
         val connectedNodes = graph(n)
 
         connectedNodes.foreach { node =>
-          if (otherNodes.contains(node)) {
-            foundValue = true
-          } else {
-            if (!visited.contains(node)) {
-              queue.enqueue(node)
-              visited.addOne(node)
-              output = output + 1
-            }
+          if (otherNodes.contains(node))
+            isNearestClone = true
+          else if (!visited.contains(node)) {
+            queue.enqueue(node)
+            visited.addOne(node)
+            output += 1
           }
         }
       }
       output
-    }
+    } else -1
   }
 
   def main(args: Array[String]): Unit = {
     val output = findShortest(
       5,
-      Array(1,1,2,3),
-      Array(2,3,4,5),
-      Array(1,2,3,3,2),
+      Array(1, 1, 2, 3),
+      Array(2, 3, 4, 5),
+      Array(1, 2, 3, 3, 2),
       value = 2)
 
 
