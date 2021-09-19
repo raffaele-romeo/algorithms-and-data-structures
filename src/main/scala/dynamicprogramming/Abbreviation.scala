e7e4d2c767d55542e70b389acbd986fe95829108
raffaele-romeo <raffaele.romeo.91.rr@gmail.com>
2021-09-19 15:47:31 +0100

    Abbreviation imrpovments

--- src/main/scala/dynamicprogramming/Abbreviation.scala
+++ src/main/scala/dynamicprogramming/Abbreviation.scala
@@ -8,14 +8,20 @@ object Abbreviation {
     var j = 0
 
     while (a.length >= b.length && isItPossibleToBeEqual && i < a.length && j < b.length) {
-      if (a(i).toUpper == b(j)) {
+      if (a(i) == b(j)) {
         i += 1
         j += 1
       } else {
-        if (Character.isUpperCase(a(i)))
+        if (Character.isUpperCase(a(i))) {
           isItPossibleToBeEqual = false
-        else
+        } else if (i + 1 < a.length && a(i + 1) == b(j))
           i += 1
+        else if(a(i).toUpper == b(j)) {
+          i += 1
+          j += 1
+        } else {
+          i += 1
+        }
       }
     }
 
@@ -34,8 +40,8 @@ object Abbreviation {
   }
 
   def main(args: Array[String]): Unit = {
-    val a = "ABCaaa"
-    val b = ""
+    val a = "gGIR"
+    val b = "GIR"
 
     println(abbreviation(a, b))
   }
