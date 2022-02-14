f435d2610874e3566ef084c4a8813c5ec2015607
raffaele-romeo <raffaele.romeo.91.rr@gmail.com>
2022-02-14 18:37:47 +0000

    Add tripleSum solution

--- src/main/scala/search/TripleSum.scala
+++ src/main/scala/search/TripleSum.scala
@@ -1,5 +1,22 @@
 package search
 
 object TripleSum extends App {
+  def triplets(a: Array[Int], b: Array[Int], c: Array[Int]): Long = {
+    val aSet = a.distinct.sorted
+    val bSet = b.distinct.sorted
+    val cSet = c.distinct.sorted
 
+    var count = 0L
+    var m = 0
+    var n = 0
+
+    for (bElem <- bSet) {
+      while (m < aSet.length && aSet(m) <= bElem) m += 1
+      while (n < cSet.length && cSet(n) <= bElem) n += 1
+
+      count += m * n
+    }
+
+    count
+  }
 }
